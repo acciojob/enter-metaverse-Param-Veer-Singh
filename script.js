891a94c7d5ad1a7d516e93250eef03d4799edf5f
Param-Veer-Singh <122340319+Param-Veer-Singh@users.noreply.github.com>
2023-10-25 13:33:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+const enterBtn = document.getElementById("enterBtn");
+
+function onclick(){
+    const para = document.getElementById("status");
+    const newHeading = document.createElement("h1");
+    newHeading.innerText = "Entered Metaverse";
+    para.replaceWith(newHeading);
+}
+enterBtn.addEventListener("click",onclick);
